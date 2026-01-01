7f527b9612b20f56c5b3e919c210fe2150781ccf
AdamPierce639512 <adampierce639512@gmail.com>
2026-01-01 15:26:03 -0500

    removed block formatting from raid end

--- Attendance.lua
+++ Attendance.lua
@@ -154,7 +154,7 @@ local function MessageRaidEndCSV()
 end
 
 local function MessageRaidEnd()
-	MessageSquadAttendance("> ______Raid Ended [".. date("%m-%d %H:%M")  .."] Attendees:______")
+	MessageSquadAttendance("______Raid Ended [".. date("%m-%d %H:%M")  .."] Attendees:______")
 
 	if not RaidData then
 		print("No raid data available.")
